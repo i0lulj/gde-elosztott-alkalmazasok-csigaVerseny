b45c329c01913969109979d51e0895df630c10fa
Péter Birkás <il0ulj@neptun.gde.hu>
2024-05-02 11:39:42 +0200

    author modified, task description added

--- src/csigaverseny/CsigaVerseny.java
+++ src/csigaverseny/CsigaVerseny.java
@@ -5,9 +5,19 @@
 package csigaverseny;
 
 /**
- *
- * @author user
+ * 
+ * @author Péter Birkás NK: il0ulj
+ * 
+ * Az alábbi feladatot kell elkészíteni OOP szemlélettel, Git-re feltöltve:
+ * A játékban 3 csiga versenyzik 5 körön keresztül, az nyer, 
+ * aki messzebb jut. Egy körben, egy csiga 0..3 egységet megy, véletlenszerűen. 
+ * A csigáknak van színe: piros zöld, kék.
+ * A játék elején lehet fogadni, hogy melyik csiga nyer!
+ * A játékban minden körben 20% eséllyel kiválasztásra kerül egy csiga, 
+ * aki kap csigagyorsítót! Ennek hatására abban a körben dupla lesz a sebessége!
+ * 
  */
+
 public class CsigaVerseny {
 
     /**
